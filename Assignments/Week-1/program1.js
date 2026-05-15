4f897936c1308a251caeea6b33ba01f1d0f06bb8
abhii66 <abhireddy0137@gmail.com>
2026-05-15 13:40:25 +0530

    Refactor program1.js for better readability
    Updated variable names and added comments for clarity.

--- Assignments/Week-1/program1.js
+++ Assignments/Week-1/program1.js
@@ -1,7 +1,9 @@
 // Find the biggest of 2 numbers 
-let a=10
-let b=20
-if(a>>b)
-    console.log(`${a} is bigger.`)
+let num1=10
+let num2=20
+//checking the condition, if yes-
+if(num1>>num2)
+    console.log(`${num1} is bigger.`)
+//else statement
 else
-    console.log(`${b} is bigger.`)
+    console.log(`${num2} is bigger.`)
