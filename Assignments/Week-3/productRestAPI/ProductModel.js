9ad2432e427edad14e1fef400a54af61bf19e58e
abhii66 <abhireddy0137@gmail.com>
2026-05-15 14:21:08 +0530

    Refactor ProductModel with unique constraint on productId

--- Assignments/Week-3/productRestAPI/ProductModel.js
+++ Assignments/Week-3/productRestAPI/ProductModel.js
@@ -1,29 +1,31 @@
-import { Schema,model } from "mongoose";
+// Product Model Definition
+import { Schema, model } from "mongoose";
 
-const productSchema=new Schema({
-    productId:{
-        type:Number,
-        required: [true, "product id is required."],
-        //unique:[true,"Id already exists"]
-    },
-    productName:{
-        type:String,
-        required:[true,"Product name is required."]
-    },
-    price:{
-        type:Number,
-        required:[true,"Price is required."],
-        min:[10000,"Price should atleast be 10000"],
-        max:[50000,"Price can only be upto 50000"]
-    },
-    brand:{
-        type:String,
-        required:[true,"Brand Name is required."]
-    }
-},
-{
-    versionKey:false,
-    timestamps:true
-})
+// Define Schema
+const productSchema = new Schema({
+  productId: {
+    type: Number,
+    required: [true, "product id is required."],
+    unique: [true, "Id already exists"]
+  },
+  productName: {
+    type: String,
+    required: [true, "Product name is required."]
+  },
+  price: {
+    type: Number,
+    required: [true, "Price is required."],
+    min: [10000, "Price should atleast be 10000"],
+    max: [50000, "Price can only be upto 50000"]
+  },
+  brand: {
+    type: String,
+    required: [true, "Brand Name is required."]
+  }
+}, {
+  versionKey: false,
+  timestamps: true
+});
 
-export const ProductModel=model("product",productSchema) 
+// Create and export model
+export const ProductModel = model("product", productSchema);
