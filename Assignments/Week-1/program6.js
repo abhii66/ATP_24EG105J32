b98970743a64f8fe5a52e8f041210b4367316ac5
abhii66 <abhireddy0137@gmail.com>
2026-05-15 13:54:35 +0530

    Add comments to sumOfArray function
    Added comments to clarify the purpose of the function and its execution.

--- Assignments/Week-1/program6.js
+++ Assignments/Week-1/program6.js
@@ -1,9 +1,12 @@
+// Calculates the total sum of all numerical elements within an array.
 let exampleArray=[10,30,20]
 function sumOfArray(exampleArray){
     let sum=0
+    //loop to calculate the sum
     for(let i=0;i<exampleArray.length;i++)
     sum+=exampleArray[i]
     return sum
 }
+// Execute function and store the result
 let result=sumOfArray(exampleArray)
 console.log(`${result} is sum of the Array.`)
