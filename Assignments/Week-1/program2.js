5f0d5e64769dfcd4fb7921847684f08f4935f8d2
abhii66 <abhireddy0137@gmail.com>
2026-05-15 13:43:09 +0530

    Refactor program to use descriptive variable names

--- Assignments/Week-1/program2.js
+++ Assignments/Week-1/program2.js
@@ -1,10 +1,12 @@
 //Find the biggest of 3 numbers 
-let a=10
-let b=20
-let c=13
-if(a>b && a>c)
-    console.log(`${a} is bigger.`)
-else if(b>c && b>a)
-    console.log(`${b} is bigger.`)
+//variable declarations
+let num1=10
+let num2=20
+let num3=13
+//conditions check
+if(num1>num2 && num1>num3)
+    console.log(`${num1} is bigger.`)
+else if(num2>num3 && num2>num1)
+    console.log(`${num2} is bigger.`)
 else 
-    console.log(`${c} is bigger.`)
+    console.log(`${num3} is bigger.`)
